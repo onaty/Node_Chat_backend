10bd5369a78a8d3557985dceed0a4668c30ce398
Oluwatobi Onasanya <onatty98@gmail.com>
2019-01-26 15:18:50 +0100

    mongo

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://onaty:onaty@cluster0-shard-00-00-mhmoo.mongodb.net:27017,cluster0-shard-00-01-mhmoo.mongodb.net:27017,cluster0-shard-00-02-mhmoo.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+  database: 'mongodb+srv://onaty:onaty@cluster0-mhmoo.mongodb.net/test?retryWrites=true',
   secret:'a1j2t3'
 }
