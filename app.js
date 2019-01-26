0849bcbe9c7276e8758b63f42b7ea73c21225a54
Oluwatobi Onasanya <onatty98@gmail.com>
2019-01-26 07:28:54 +0100

    downgrage mongoose

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ app.use(bodyParser.urlencoded({
 
 // var store = new MongoDBStore({
 //   uri: 'mongodb://onaty:onaty@cluster0-shard-00-00-mhmoo.mongodb.net:27017,cluster0-shard-00-01-mhmoo.mongodb.net:27017,cluster0-shard-00-02-mhmoo.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
-//   databaseName: 'chatapp',
-//   collection: 'mySessions'
+//   // databaseName: 'chatapp',
+//   // collection: 'mySessions'
 // });
 // app.use(require('express-session')({
 //   secret: 'This is a secret',
